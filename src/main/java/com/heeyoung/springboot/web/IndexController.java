0013535d1f250ec7402765c49463e815615f54d2
klloo <asdf016182@naver.com>
2021-02-21 22:58:15 +0900

    Day4 구글, 네이버 로그인 추가

--- src/main/java/com/heeyoung/springboot/web/IndexController.java
+++ src/main/java/com/heeyoung/springboot/web/IndexController.java
@@ -1,6 +1,8 @@
 package com.heeyoung.springboot.web;
 
 
+import com.heeyoung.springboot.config.auth.LoginUser;
+import com.heeyoung.springboot.config.auth.dto.SessionUser;
 import com.heeyoung.springboot.service.posts.PostsService;
 import com.heeyoung.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import javax.servlet.http.HttpSession;
+
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
-    
+
     private final PostsService postsService;
+    private final HttpSession httpSession;
+
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts",postsService.findAllDesc());
+        model.addAttribute("len",postsService.findAllDesc().size());
+
+        if(user!=null){
+            model.addAttribute("userImage",user.getPicture());
+            model.addAttribute("userName",user.getName());
+        }
         return "index";
     }
 
